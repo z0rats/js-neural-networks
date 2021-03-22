52da282cbe58b904d8f7ea4d0115872c87f4db98
Alexander <v0xt.alex@gmail.com>
2021-03-22 18:09:19 +0300

    Update!

---
+++ multilayer_net/iris.js
@@ -0,0 +1,61 @@
+import path from 'path';
+import _ from 'lodash';
+import dataNormalization from '../utils/dataNormalization.js';
+import { extractLastColumn, smallValuesArr, readCsv } from '../utils/utils.js';
+import NeuralNetwork from './core/NeuralNetwork.js';
+
+(async () => {
+  const pathToTrain = path.join(process.cwd(), 'datasets/iris_train.csv');
+  const pathToTest = path.join(process.cwd(), 'datasets/iris_test.csv');
+  const trainData = await readCsv(pathToTrain);
+  const testData = await readCsv(pathToTest);
+
+  const trainExpected = extractLastColumn(trainData);
+  const testExpected = extractLastColumn(testData);
+  const output = _.uniq(trainExpected).sort();
+
+  const normalisedTestData = dataNormalization.normalize.minMaxNegative(testData);
+  const normalisedTrainData = dataNormalization.normalize.minMaxNegative(trainData);
+
+  const learningRate = 0.1;
+  const inputNodes = 4;
+  const outputNodes = output.length;
+  const layers = [inputNodes, 500, 250, 125, outputNodes];
+  const net = new NeuralNetwork(layers, learningRate, 'softSign');
+
+  const start = new Date();
+  const hrstart = process.hrtime();
+  console.log('Старт обучения: ', start.toTimeString());
+  for (let epoch = 0; epoch < 1; epoch += 1) {
+    normalisedTrainData.forEach((record, i) => {
+      const targets = smallValuesArr(outputNodes);
+      targets[trainExpected[i]] = 0.99;
+      net.train(record, targets);
+    });
+  }
+
+  const end = new Date() - start;
+  const hrend = process.hrtime(hrstart);
+  console.log('Время исполнения: %dms ', end);
+  console.log('Время исполнения (hr): %ds %dms ', hrend[0], hrend[1] / 1000000);
+
+  let wrongCount = 0;
+  normalisedTestData.forEach((record, i) => {
+    net.init(record);
+    const result = net.runSigmoid();
+
+    const maxVal = Math.max(...result);
+    const prediction = result.indexOf(maxVal);
+    const expected = testExpected[i];
+
+    // console.log('exp', expected);
+    // console.log('predict', result);
+
+    if (prediction !== Number(expected)) wrongCount += 1;
+  });
+  console.log(wrongCount);
+  console.log(testData.length);
+
+  const accuracy = 100 - (wrongCount / testData.length) * 100;
+  console.log({ inputsCount: testData.length, wrongCount, accuracy });
+})();
---
+++ multilayer_net/mnist_small.js
@@ -0,0 +1,55 @@
+import path from 'path';
+import _ from 'lodash';
+import { extractFirstColumn, smallValuesArr, readCsv } from '../utils/utils.js';
+import NeuralNetwork from './core/NeuralNetwork.js';
+
+(async () => {
+  const pathToTrain = path.join(process.cwd(), 'datasets/mnist_train_100.csv');
+  const pathToTest = path.join(process.cwd(), 'datasets/mnist_test_10.csv');
+  const trainData = await readCsv(pathToTrain);
+  const testData = await readCsv(pathToTest);
+
+  const trainExpected = extractFirstColumn(trainData);
+  const testExpected = extractFirstColumn(testData);
+  const output = _.uniq(trainExpected).sort();
+
+  const learningRate = 0.1;
+  const inputNodes = 784;
+  const outputNodes = output.length;
+  const layers = [inputNodes, 250, 125, outputNodes];
+  const net = new NeuralNetwork(layers, learningRate, 'th');
+
+  const start = new Date();
+  const hrstart = process.hrtime();
+  console.log('Старт обучения: ', start.toTimeString());
+  for (let epoch = 0; epoch < 10; epoch += 1) {
+    trainData.forEach((record, i) => {
+      const inputs = record.map((x) => (x / 255) * 0.99 + 0.01);
+      const targets = smallValuesArr(outputNodes);
+      targets[trainExpected[i]] = 0.99;
+      net.train(inputs, targets);
+    });
+  }
+  const end = new Date() - start;
+  const hrend = process.hrtime(hrstart);
+  console.log('Время исполнения: %dms ', end);
+  console.log('Время исполнения (hr): %ds %dms ', hrend[0], hrend[1] / 1000000);
+
+  let wrongCount = 0;
+  testData.forEach((record, i) => {
+    const inputs = record.map((x) => (x / 255) * 0.99 + 0.01);
+    net.init(inputs);
+    const result = net.runSigmoid();
+
+    const maxVal = Math.max(...result);
+    const prediction = result.indexOf(maxVal);
+    const expected = testExpected[i];
+
+    if (prediction !== Number(expected)) wrongCount += 1;
+  });
+  console.log(wrongCount);
+  console.log(testData.length);
+
+  const accuracy = 100 - (wrongCount / testData.length) * 100;
+  console.log({ 'Количество тестовых примеров:': testData.length, 'Количество ошибок:': wrongCount, 'Точность %:': accuracy });
+})();
